e38c539ba265cb12e24f28ba4a55594d34621946
Gustavo Domingueti <gustavo.domingueti@skeelo.app>
2022-09-22 20:22:44 -0300

    feat: update home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,67 @@
+import {
+  Flex,
+  Input,
+  Button,
+  Stack,
+  FormLabel,
+  FormControl,
+} from '@chakra-ui/react';
+
 export default function Home() {
-  return <h1>dashgo</h1>;
+  return (
+    <Flex
+      width="100vw"
+      height="100vh"
+      alignItems="center"
+      justifyContent="center"
+    >
+      <Flex
+        as="form"
+        width="100%"
+        maxWidth={360}
+        background="gray.800"
+        padding="8"
+        borderRadius={8}
+        flexDirection="column"
+      >
+        <Stack spacing="4">
+          <FormControl>
+            <FormLabel htmlFor="email">E-mail</FormLabel>
+            <Input
+              type="email"
+              id="email"
+              name="email"
+              focusBorderColor="pink.500"
+              background="gray.900"
+              variant="filled"
+              size="lg"
+              _hover={{
+                background: 'gray.900',
+              }}
+            />
+          </FormControl>
+
+          <FormControl>
+            <FormLabel htmlFor="password">Senha</FormLabel>
+            <Input
+              type="password"
+              id="password"
+              name="password"
+              focusBorderColor="pink.500"
+              background="gray.900"
+              variant="filled"
+              size="lg"
+              _hover={{
+                background: 'gray.900',
+              }}
+            />
+          </FormControl>
+        </Stack>
+
+        <Button type="submit" marginTop="6" colorScheme="pink">
+          Entrar
+        </Button>
+      </Flex>
+    </Flex>
+  );
 }
