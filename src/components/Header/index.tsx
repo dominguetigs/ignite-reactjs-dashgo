adc4312d8fc9984b296c46be021df5690baea46d
Gustavo Domingueti <gustavo.domingueti@skeelo.app>
2022-09-26 22:09:41 -0300

    feat(contexts): create SidebarDrawerContext

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,10 +1,14 @@
-import { Flex, useBreakpointValue } from '@chakra-ui/react';
+import { Flex, Icon, IconButton, useBreakpointValue } from '@chakra-ui/react';
+import { RiMenuLine } from 'react-icons/ri';
+import { useSidebarDrawer } from '../../contexts/SidebarDrawerContext';
 import { Logo } from './Logo';
 import { NotificationsNav } from './NotificationsNav';
 import { Profile } from './Profile';
 import { SearchBox } from './SearchBox';
 
 export const Header = (): JSX.Element => {
+  const { onOpen } = useSidebarDrawer();
+
   const isWideVersion = useBreakpointValue({
     base: false,
     lg: true,
@@ -21,6 +25,17 @@ export const Header = (): JSX.Element => {
       paddingX="6"
       alignItems="center"
     >
+      {!isWideVersion && (
+        <IconButton
+          aria-label="Open navigation"
+          icon={<Icon as={RiMenuLine} />}
+          fontSize="24"
+          variant="unstyled"
+          mr="2"
+          onClick={onOpen}
+        />
+      )}
+
       <Logo />
 
       {isWideVersion && <SearchBox />}
