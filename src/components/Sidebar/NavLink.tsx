7c874f4d6be6f9b547c2d14cf8f736951abe3991
Gustavo Domingueti <gustavo.domingueti@skeelo.app>
2022-09-26 22:54:13 -0300

    feat: create ActiveLink component

--- src/components/Sidebar/NavLink.tsx
+++ src/components/Sidebar/NavLink.tsx
@@ -1,27 +1,34 @@
 import {
   Icon,
-  Link,
+  Link as ChakraLink,
   Text,
   LinkProps as ChakraLinkProps,
 } from '@chakra-ui/react';
 import { ElementType } from 'react';
 
+import Link from 'next/link';
+import { ActiveLink } from '../ActiveLink';
+
 interface NavLinkProps extends ChakraLinkProps {
   icon: ElementType;
+  href: string;
   children: string;
 }
 
 export const NavLink = ({
   icon,
+  href,
   children,
   ...rest
 }: NavLinkProps): JSX.Element => {
   return (
-    <Link display="flex" alignItems="center" {...rest}>
-      <Icon as={icon} fontSize="20" />
-      <Text marginLeft="4" fontWeight="medium">
-        {children}
-      </Text>
-    </Link>
+    <ActiveLink href={href} passHref>
+      <ChakraLink display="flex" alignItems="center" {...rest}>
+        <Icon as={icon} fontSize="20" />
+        <Text marginLeft="4" fontWeight="medium">
+          {children}
+        </Text>
+      </ChakraLink>
+    </ActiveLink>
   );
 };
