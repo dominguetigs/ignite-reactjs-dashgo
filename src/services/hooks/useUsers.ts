00adeed6d369307c1e1e834110ea605760cfe784
Gustavo Domingueti <gustavo.domingueti@skeelo.app>
2022-10-26 21:13:31 -0300

    feat: update useUsers hook

--- src/services/hooks/useUsers.ts
+++ src/services/hooks/useUsers.ts
@@ -8,16 +8,28 @@ type User = {
   createdAt: string;
 };
 
-export async function getUsers(): Promise<User[]> {
-  const { data } = await api.get('users');
+type GetUsersResponse = {
+  totalCount: number;
+  users: User[];
+};
 
-  console.log(data);
+export async function getUsers(page: number): Promise<GetUsersResponse> {
+  const { data, headers } = await api.get('users', {
+    params: {
+      page,
+    },
+  });
 
-  return data.users;
+  const totalCount = Number(headers['x-total-count']);
+
+  return {
+    users: data.users,
+    totalCount,
+  };
 }
 
-export function useUsers() {
-  return useQuery(['users'], getUsers, {
+export function useUsers(page: number) {
+  return useQuery(['users', page], () => getUsers(page), {
     staleTime: 1000 * 5, // 5 seconds
   });
 }
