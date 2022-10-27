8ba06f0c7447c9ffd7328a3f20cb1d0bc8fb8172
Gustavo Domingueti <gustavo.domingueti@skeelo.app>
2022-10-26 21:22:42 -0300

    feat: set prefetch to user detail

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -6,12 +6,11 @@ import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 
 import { theme } from '../styles/theme';
+import { queryClient } from '../services/queryClient';
 
 if (process.env.NODE_ENV === 'development') {
   makeServer();
 }
-
-const queryClient = new QueryClient();
 
 function MyApp({ Component, pageProps }) {
   return (
